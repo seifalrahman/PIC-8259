e1e6c8bfd4bb994cc475d404863b3f14f2738012
Ahmedgamalsoli <121308198+Ahmedgamalsoli@users.noreply.github.com>
2023-12-07 00:18:17 +0200

    Add files via upload

---
+++ DataBuffer.v
@@ -0,0 +1,13 @@
+module DataBuffer (
+  inout wire [7:0] D, InternalD,
+  input wire R, W
+  ,output reg [7:0] buffer
+);
+always @(*)
+begin
+	buffer = (R == 0 && W == 1) ? InternalD : buffer;//read
+	buffer = (R == 1 && W == 0) ? D : buffer;//write
+end
+  assign D = (R == 0 && W == 1) ? buffer : 8'bzzzzzzzz;//read
+  assign InternalD = (R == 1 && W == 0) ? buffer : 8'bzzzzzzzz;//write
+endmodule

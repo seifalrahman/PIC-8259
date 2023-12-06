c2ab5e047f70a78c8ed1a2c1a1057a90094ef8a2
seifalrahman <130805731+seifalrahman@users.noreply.github.com>
2023-12-07 00:23:43 +0200

    Update IRR.v

--- IRR.v
+++ IRR.v
@@ -1,4 +1,7 @@
 module Interrupt_Request(
+
+    // Inputs from control logic
+    
     //1 is for level , 0 is for edge
     input wire edge_level_config,
     
@@ -9,25 +12,34 @@ module Interrupt_Request(
     input wire [7:0] interrupt_req_pin,
 
     // Outputs
-    output reg [7:0] interrupt_req_register
+    output reg [7:0] interrupt_req_register = 8'b00000000
 );
-
-    genvar ir_bit_no;
     
-    generate for (ir_bit_no = 0; ir_bit_no <= 7; ir_bit_no = ir_bit_no + 1) begin: Request_Latch
-      
-      if(!edge_level_config) begin
-        always @(posedge interrupt_req_pin[ir_bit_no],clear_interrupt_req[ir_bit_no])  begin
+    genvar ir_bit_no;
         
-            if(clear_interrupt_req[ir_bit_no])  
-                interrupt_req_register[ir_bit_no] <= 1'b0;
-            else if(freeze) 
-                interrupt_req_register[ir_bit_no]=interrupt_req_register[ir_bit_no];
-            else 
-                interrupt_req_register[ir_bit_no] <= 1'b1;
-             
+    generate for (ir_bit_no = 0; ir_bit_no <= 7; ir_bit_no = ir_bit_no + 1) begin: Request_Latch
+        
+        always @(*)  begin
+          if(!edge_level_config) begin
+              if(clear_interrupt_req[ir_bit_no])  
+                  interrupt_req_register[ir_bit_no] <= 1'b0;
+              else if(freeze) 
+                  interrupt_req_register[ir_bit_no]=interrupt_req_register[ir_bit_no];
+              else if(interrupt_req_pin[ir_bit_no]==1)
+                  interrupt_req_register[ir_bit_no] <= 1'b1;
+              else 
+                  interrupt_req_register[ir_bit_no]=interrupt_req_register[ir_bit_no];
+          end
+          
+          else begin
+         if(clear_interrupt_req[ir_bit_no])
+            interrupt_req_register[ir_bit_no] <= 1'b0;
+         else
+                      interrupt_req_register[ir_bit_no] <= interrupt_req_pin[ir_bit_no];
+          end   
         end
-      end
+         
+    end
     endgenerate
 
-endmodule
+endmodule   

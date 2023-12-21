9c528eee61bcc836dda219cb083d642f8263ad41
Waleed Alzamil <95117901+WaleedAlzamil80@users.noreply.github.com>
2023-12-21 20:18:00 +0200

    modified DataBuffer

--- DataBuffer.v
+++ DataBuffer.v
@@ -1,13 +1,15 @@
 module DataBuffer (
-  inout wire [7:0] D, InternalD,
-  input wire R, W
-  ,output reg [7:0] buffer
+             inout wire     [7:0]     Data    ,     
+           inout wire     [7:0]    InternalD,
+             input wire         R    ,
+           input wire         W        
 );
+reg [7:0] buffer;
 always @(*)
 begin
-	buffer = (R == 0 && W == 1) ? InternalD : buffer;//read
-	buffer = (R == 1 && W == 0) ? D : buffer;//write
+    buffer = (R == 0 && W == 1) ? InternalD : buffer;//read
+    buffer = (R == 1 && W == 0) ? Data : buffer;//write
 end
-	assign D = (R == 0 && W == 1) ? buffer : 8'bzzzzzzzz;//CPU read
-	assign InternalD = (R == 1 && W == 0) ? buffer : 8'bzzzzzzzz;//CPU write
+  assign Data = (R == 0 && W == 1) ? buffer : 8'bzzzzzzzz;//read
+  assign InternalD = (R == 1 && W == 0) ? buffer : 8'bzzzzzzzz;//write
 endmodule

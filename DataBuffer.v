6eaa4b039b6e04e3c2b10989514544f0e2618511
Ahmedgamalsoli <121308198+Ahmedgamalsoli@users.noreply.github.com>
2023-12-21 22:03:07 +0200

    Update DataBuffer.v

--- DataBuffer.v
+++ DataBuffer.v
@@ -1,15 +1,16 @@
 module DataBuffer (
-             inout wire     [7:0]     Data    ,     
-           inout wire     [7:0]    InternalD,
-             input wire         R    ,
-           input wire         W        
+             inout wire     [7:0]     Data        ,     
+           inout wire     [7:0]    InternalD    ,
+             input wire         R        ,
+           input wire         W        ,
+           input wire        Flag_From_Cascade 
 );
 reg [7:0] buffer;
 always @(*)
 begin
-    buffer = (R == 0 && W == 1) ? InternalD : buffer;//read
+    buffer = ((R == 0 && W == 1) || Flag_From_Cascade == 1) ? InternalD : buffer;//read
     buffer = (R == 1 && W == 0) ? Data : buffer;//write
 end
-  assign Data = (R == 0 && W == 1) ? buffer : 8'bzzzzzzzz;//read
+  assign Data = ((R == 0 && W == 1) || Flag_From_Cascade == 1) ? buffer : 8'bzzzzzzzz;//read
   assign InternalD = (R == 1 && W == 0) ? buffer : 8'bzzzzzzzz;//write
 endmodule

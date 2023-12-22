51046610493676c671c3d4d3c107c80ca793f45b
seifalrahman <130805731+seifalrahman@users.noreply.github.com>
2023-12-23 00:02:04 +0200

    Update DataBuffer.v

--- DataBuffer.v
+++ DataBuffer.v
@@ -1,16 +1,24 @@
 module DataBuffer (
-  		   inout wire 	[7:0] 	Data		, 	
-		   inout wire 	[7:0]	InternalD	,
-  		   input wire 		R		,
-		   input wire	 	W		,
-		   input wire		Flag_From_Cascade 
+             input wire     [7:0]     CPU_IN_Data    ,
+           output wire     [7:0]    CPU_OUT_Data    ,
+           input wire     [7:0]    IN_InternalD    ,
+           output wire    [7:0]    OUT_InternalD    ,     
+             input wire         R        ,
+           input wire         W        ,
+           input wire        Flag_From_Cascade 
 );
-reg [7:0] buffer;
+reg [7:0] CPU_OUT_Data_reg , OUT_InternalD_reg;
 always @(*)
 begin
-	buffer = ((R == 0 && W == 1) || Flag_From_Cascade == 1) ? InternalD : buffer;//read
-	buffer = (R == 1 && W == 0) ? Data : buffer;//write
+    if((R == 0 && W == 1) || Flag_From_Cascade == 1)
+        CPU_OUT_Data_reg =IN_InternalD ;//read
+    else if (R == 1 && W == 0)
+        OUT_InternalD_reg = CPU_IN_Data ;//write
+    else begin // saty in the same value
+        CPU_OUT_Data_reg = CPU_OUT_Data_reg;
+        OUT_InternalD_reg = OUT_InternalD_reg;
+         end
 end
-  assign Data = ((R == 0 && W == 1) || Flag_From_Cascade == 1) ? buffer : 8'bzzzzzzzz;//read
-  assign InternalD = (R == 1 && W == 0) ? buffer : 8'bzzzzzzzz;//write
+  assign CPU_OUT_Data = CPU_OUT_Data_reg;
+  assign OUT_InternalD = OUT_InternalD_reg;
 endmodule

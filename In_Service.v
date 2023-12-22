508ff53f924cba5ca51db0fe726a9c2f40af00ee
Seif-Gama1 <126584186+Seif-Gama1@users.noreply.github.com>
2023-12-23 00:00:45 +0200

    Update In_Service.v

--- In_Service.v
+++ In_Service.v
@@ -8,12 +8,12 @@ module In_Service(
     input      [7:0]   end_of_interrupt,  
 
     // Outputs
-    output  reg     [7:0]   in_service_register,
+    output  reg     [7:0]   in_service_register = 8'b00000000,
     output  reg     [7:0]   highest_level_in_service
   
   );
   
-    wire    [7:0]   next_highest_level_in_service_wire1;
+    reg    [7:0]   next_highest_level_in_service;
     
   //
   // In service register
@@ -23,29 +23,42 @@ module In_Service(
                                      | (latch_in_service == 1'b1 ? interrupt : 8'b00000000);
   end
   
+  wire [7:0] final_next_highest_level_in_service;
+  wire [7:0] rotated_next_highest_level_in_service, resolved_next_highest_level_in_service;
+  
+  wire [7:0] ISR_SpecialMasked;
+  
+  assign ISR_SpecialMasked = in_service_register & ~interrupt_special_mask;
+  
+  always @(*) begin
+      // (ISR) & (special mask)
+      //rotate right     (Implemented by module below)
+      //Resolve Priority (Implemented by module below)
+      //rotate left      (Implemented by module below)
+      next_highest_level_in_service = final_next_highest_level_in_service;  
+  end
+
+
   rotate_right rotate_R1(
-      .source(next_highest_level_in_service),
+      .source(ISR_SpecialMasked),
       .rotate(priority_rotate),
-      .rotated_R_output(next_highest_level_in_service)
+      .rotated_R_output(rotated_next_highest_level_in_service)
+  );
+  
+  
+  resolve_priority RP(
+      .source(rotated_next_highest_level_in_service),
+      .resolved_priority(resolved_next_highest_level_in_service)
   );
   
   rotate_left rotate_L1(
-      .source(next_highest_level_in_service),
+      .source(resolved_next_highest_level_in_service),
       .rotate(priority_rotate),
-      .rotated_L_output(next_highest_level_in_service)
+      .rotated_L_output(final_next_highest_level_in_service)
   );  
 
-
-assign next_highest_level_in_service_wire1 = in_service_register & ~interrupt_special_mask;
-
-wire [7:0] next_highest_level_in_service_wire2;
-  resolve_priority RP(
-    .source(next_highest_level_in_service_wire1),
-    .resolved_priority(next_highest_level_in_service_wire2)
-  );
-
   always @(*) begin
-          highest_level_in_service <= next_highest_level_in_service_wire2;
+          highest_level_in_service <= next_highest_level_in_service;
   end
 
 endmodule

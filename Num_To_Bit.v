70205b32663490019ac0e32f383642a5f9281af2
Waleed Alzamil <95117901+WaleedAlzamil80@users.noreply.github.com>
2023-12-22 13:30:25 +0200

    modify num to bit

--- Num_To_Bit.v
+++ Num_To_Bit.v
@@ -1,19 +1,15 @@
 module Num_To_Bit (
     input [2:0] source,
-    output reg [7:0] num2bit
+    output wire [7:0] num2bit
 );
 
-    always @(*)
-        case (source)
-            3'b000:  num2bit = 8'b00000001;
-            3'b001:  num2bit = 8'b00000010;
-            3'b010:  num2bit = 8'b00000100;
-            3'b011:  num2bit = 8'b00001000;
-            3'b100:  num2bit = 8'b00010000;
-            3'b101:  num2bit = 8'b00100000;
-            3'b110:  num2bit = 8'b01000000;
-            3'b111:  num2bit = 8'b10000000;
-            default: num2bit = 8'b00000000;
-        endcase
+    assign num2bit = (source == 3'b000) ? 8'b00000001 :
+                    (source == 3'b001) ? 8'b00000010 :
+                    (source == 3'b010) ? 8'b00000100 :
+                    (source == 3'b011) ? 8'b00001000 :
+                    (source == 3'b100) ? 8'b00010000 :
+                    (source == 3'b101) ? 8'b00100000 :
+                    (source == 3'b110) ? 8'b01000000 :
+                    (source == 3'b111) ? 8'b10000000 : 8'b00000000;
 
 endmodule

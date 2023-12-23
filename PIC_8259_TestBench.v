024215b552588136342864adab65cfb1ea49dd36
mostafa0110 <73003302+mostafa0110@users.noreply.github.com>
2023-12-23 22:57:33 +0200

    Update PIC_8259_TestBench.v

--- PIC_8259_TestBench.v
+++ PIC_8259_TestBench.v
@@ -1,168 +1,4 @@
 `timescale 1ns /100ps
-module PIC_8259A (
-		  input wire [7:0]  	D_IN 	,
-		  output wire [7:0]	D_OUT	,
-		  inout wire [2:0]	CAS	,
-		  inout wire 		SP_EN_n	,
-		  input wire		RD_n 	, 
-		  input wire		WR_n 	,
-		  input wire		A0 	,
-		  input wire		CS_n	,
-		  input wire [7:0]	IR 	,
-		  input wire		INTA_n	,
-		  output wire		INT	
-		);
-//internal_Bus
-wire [7:0] InternalData_IN , InternalData_OUT;
-
-//Cascade-->DataBuffer
-wire Flag_From_Cascade;
-
-DataBuffer DataBusBuffer(
-	.CPU_IN_Data(D_IN)			,
-	.CPU_OUT_Data(D_OUT)			,
-	.IN_InternalD(InternalData_IN)		,
-	.OUT_InternalD(InternalData_OUT)	,
-	.R(RD_n)				,
-	.W(WR_n)				,
-	.Flag_From_Cascade(Flag_From_Cascade)
-	);
-
-
-//ReadWrite-->Control
-wire [7:0] W_Data_2Control;//ICWs and OCWs
-wire [2:0] W_Flag_2Control;//flags from 0 to 6
-wire [2:0] R_Flag_2Control;//flag to identifi read register
-	
-Read_WriteLogic ReadWriteLogic(
-	.RD(RD_n)					,
-	.WR(WR_n)					,
-	.A0(A0)						,
-	.CS(CS_n)					,
-	.inputRegister(InternalData_OUT)		,
-	.control_output_Register(W_Data_2Control)	,
-	.Flag(W_Flag_2Control)				,
-	.read2control(R_Flag_2Control)					
-	);
-
-
-//Control-->Cascade
-wire [7:0] ICW3Cascade	;
-wire [7:0] IRRCascade	; //it has only one bit set from ISR
-wire [7:0] ICW2Cascade	; 
-wire SP_ENCascade;	
-wire SNGL;//to check singlr or cascade mode
- 
-Cascademodule Cascade_Buffer_Comparator(
-	.CAS(CAS)					,
-	.SP_EN(SP_ENCascade)				,
-	.ICW3(ICW3Cascade)				,
-	.IRR(IRRCascade)				,
-	.ICW2(ICW2Cascade)				,	
-	.SNGL(SNGL)					,
-	.INTA(INTA_n)					,
-	.codeAddress(InternalData_IN)			,
-	.flagCodeAddress(Flag_From_Cascade)
-	);
-
-
-//Control-->IRR
-wire edge_level_config;
-wire freeze;
-wire [7:0] clear_interrupt_request;
-
-//IRR-->(Priority & Control)
-wire [7:0] IRQs_2Pri_Resolver;
-
-Interrupt_Request IRR(
-	.edge_level_config(edge_level_config)		,
-	.freeze(freeze)					,
-	.clear_interrupt_req(clear_interrupt_request)	,
-	.interrupt_req_pin(IR)				,
-	.interrupt_req_register(IRQs_2Pri_Resolver)			
-	);
-
-
-//Control-->(Priority & ISR)
-wire [2:0] priority_rotate;
-wire [7:0] interrupt_mask;
-wire [7:0] interrupt_special_mask;
-
-//ISR-->Priority
-wire [7:0] ISR_2Pri_Control;
-
-//Priority -->(Control & ISR)
-wire [7:0] InterruptID;
-
-Priority_Resolver Pri_Res(
-	.priority_rotate(priority_rotate)		,
-	.interrupt_mask(interrupt_mask)			,
-	.interrupt_special_mask(interrupt_special_mask)	,
-	.interrupt_request_register(IRQs_2Pri_Resolver)	,
-	.in_service_register(ISR_2Pri_Control)		,
-	.interrupt(InterruptID)					
-	);
-
-
-//Control-->ISR
-wire Latch;
-wire [7:0] end_of_interrupt;
-
-//ISR-->Control
-wire [7:0] highest_IS;
-
-In_Service ISR (
-	.priority_rotate(priority_rotate)		,
-	.interrupt_special_mask(interrupt_special_mask)	,
-	.interrupt(InterruptID)				,
-	.latch_in_service(Latch)			,
-	.end_of_interrupt(end_of_interrupt)		,
-	.in_service_register(ISR_2Pri_Control)		,
-	.highest_level_in_service(highest_IS)			
-	);
-
-
-
-
-Control_Logic CONTROL_LOGIC(
-	//ReadWrite-->Control
-	.ReadWriteinputData(W_Data_2Control)		,		 
-	.FlagFromRW(W_Flag_2Control)			,		
-	.read2controlRW(R_Flag_2Control)		,		
-	//internal_Bus
-	.DataBufferOutput(InternalData_IN)		,
-	//IRR-->Control
-	.IRRinput(IRQs_2Pri_Resolver)			,
-	//Control-->IRR
-	.edge_level_config(edge_level_config)		,
-	//ISR-->Control
-	.ISRinput(ISR_2Pri_Control)			,
-	.highest_level_in_service(highest_IS)		,
-	//Control-->Cascade
-	.SP_ENCascade(SP_ENCascade)			,
-	.ICW3Cascade(ICW3Cascade)			,
-	.ICW2Cascade(ICW2Cascade)			,
-	.SNGL(SNGL)					,
-	.IRRCascade(IRRCascade)				,
-	//Priority-->Control
-	.InterruptID(InterruptID)			,
-	//Top_Module
-	.INTA(INTA_n)					,
-	.INT(INT)					,
-	//Control-->(Priority & ISR)
-	.interrupt_mask(interrupt_mask)			,
-	.interrupt_special_mask(interrupt_special_mask)	,
-	.priority_rotate(priority_rotate)		,
-	//Control-->ISR
-	.end_of_interrupt(end_of_interrupt)		,
-	.latch_in_service(Latch)			,
-	//Control-->IRR
-	.freeze(freeze)					,
-	.clear_interrupt_request(clear_interrupt_request)			
-	);
-
-endmodule
-		
 module PIC_8259_TestBench ( );
 
 reg           chip_select;
@@ -173,7 +9,6 @@ reg   [7:0]    data_bus_in;
 reg   [2:0]   	  CAS;
 reg             SP_EN;
 reg           	  INTA;
-reg           	  INT;
 reg   [7:0]      IRR ;
 
 wire 	  chip_select_wire;
@@ -196,7 +31,6 @@ assign 	data_bus_wire_in = data_bus_in;
 assign 	CAS_wire = CAS;
 assign SP_EN_wire = SP_EN;
 assign INTA_wire = INTA;
-assign INT_wire = INT;
 assign IRR_wire = IRR;
 
 
@@ -287,11 +121,6 @@ begin
     TASK_WRITE_DATA(1'b0, {8'b01100, int_no});
 end
 endtask
-
-
-
-
-
 
 
 // Task : Send non-specific EOI
@@ -388,9 +217,7 @@ task TASK_8086_NORMAL_INTERRUPT_TEST();
 
         TASK_INTERRUPT_REQUEST(8'b10000000);
         TASK_SEND_ACK_TO_8086();
-        TASK_SEND_SPECIFIC_EOI(3'b111);   
-        //4
-        #10;
+        TASK_SEND_SPECIFIC_EOI(3'b111);
     end
     endtask
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -900,6 +727,4 @@ end
  always @* $monitor("At time %t: CS_n = %b, RD_n = %b, WR_n = %b, A0 = %b, CAS = %b , SP_EN_n = %b , D_IN = %b ,D_OUT = %b ,INT = %b,IR = %b",
                     $time, chip_select_wire , read_enable_wire , write_enable_wire  , A0_wire ,  CAS_wire , SP_EN_wire , data_bus_wire_in ,data_bus_wire_out ,INT_wire,IRR_wire);
 endmodule
-
-
 

1eb6f8cfc441d155cc9881a234bda6afd9871f66
aKha1ifa <131745823+aKha1ifa@users.noreply.github.com>
2023-12-24 23:58:44 +0200

    Update DataBuffer.v

--- DataBuffer.v
+++ DataBuffer.v
@@ -1,24 +1,8 @@
 module DataBuffer (
-             input wire     [7:0]     CPU_IN_Data    ,
-           output wire     [7:0]    CPU_OUT_Data    ,
-           input wire     [7:0]    IN_InternalD    ,
-           output wire    [7:0]    OUT_InternalD    ,     
-             input wire         R        ,
-           input wire         W        ,
-           input wire        Flag_From_Cascade 
+  inout wire [7:0] D, InternalD,
+  input wire R , W ,
+  input Flag_From_Cascade 
 );
-reg [7:0] CPU_OUT_Data_reg , OUT_InternalD_reg;
-always @(*)
-begin
-    if((R == 0 && W == 1) || Flag_From_Cascade == 1)
-        CPU_OUT_Data_reg =IN_InternalD ;//read
-    else if (R == 1 && W == 0)
-        OUT_InternalD_reg = CPU_IN_Data ;//write
-    else begin // saty in the same value
-        CPU_OUT_Data_reg = CPU_OUT_Data_reg;
-        OUT_InternalD_reg = OUT_InternalD_reg;
-         end
-end
-  assign CPU_OUT_Data = CPU_OUT_Data_reg;
-  assign OUT_InternalD = OUT_InternalD_reg;
+    assign D = ((R == 0 && W == 1) || Flag_From_Cascade == 1) ? InternalD : 8'bzzzzzzzz;//CPU read
+    assign InternalD = (R == 1 && W == 0) ? D : 8'bzzzzzzzz;//CPU write
 endmodule
